6576e0910212fbfa94ef8b768a8469bb52e0b26b
Juliana Ruelo <juliana.ruelo@uleth.ca>
2025-12-05 13:47:46 -0700

    got one note to change randomly on click

--- script.js
+++ script.js
@@ -2,39 +2,37 @@ console.log("connected!")
 
 var score = 0;
 
-//DROP J
-var jY = 0;
+// //DROP J
+// var jY = 0;
 
-function jDrop()    {
-    loadImage("drop.svg");
-    jY = jY +random(1, 5);
+// function jDrop()    {
+//     loadImage("drop.svg");
+//     jY = jY +random(1, 5);
 
-    if(aY > 1000)  { //400 is the bottom of the canvas
-    aY = 0;
-    }
-}
+//     if(aY > 1000)  { //400 is the bottom of the canvas
+//     aY = 0;
+//     }
+// }
 
-//drop random marimba
-var dropSound =["marimba1.mp3", "marimba2.mp3", "marimba3.mp3"] //<-- this is an array
-var counter = 0;
-//unamed function
-$("#dropSound").on("keydown", function(){
-    // // counter = counter + 1; <-- longform ver. like in processing
-    // counter++; //<-- increment operator, to add a limit smth to do w/ a link, length and percent?? which goes into the logos[counter]
-    var randomNum = Math.floor(Math.random()*dropSound.length)
-    console.log(randomNum)
-    $(this).attr("src", "assets/" + dropSound[randomNum]);
-    if(counter == dropSound.length -1)  {
-        counter = -1; //longform ver. like in processing
-    }
-})
+// //drop random marimba
+// var dropSound =["marimba1.mp3", "marimba2.mp3", "marimba3.mp3"] //<-- this is an array
+// var counter = 0;
+// //unamed function
+// $("#dropSound").on("keydown", function(){
+//     // // counter = counter + 1; <-- longform ver. like in processing
+//     // counter++; //<-- increment operator, to add a limit smth to do w/ a link, length and percent?? which goes into the logos[counter]
+//     var randomNum = Math.floor(Math.random()*dropSound.length)
+//     console.log(randomNum)
+//     $(this).attr("src", "assets/" + dropSound[randomNum]);
+//     if(counter == dropSound.length -1)  {
+//         counter = -1; //longform ver. like in processing
+//     }
+// })
 
 var notesLeft =["heart.svg", "star.svg", "drop.svg"] //<-- this is an array
 var counter = 0;
 //unamed function
-$("#notesLeft").on("keydown", function(){
-    // // counter = counter + 1; <-- longform ver. like in processing
-    // counter++; //<-- increment operator, to add a limit smth to do w/ a link, length and percent?? which goes into the logos[counter]
+$("#leftNote").on("click", function(){
     var randomNum = Math.floor(Math.random()*notesLeft.length)
     console.log(randomNum)
     $(this).attr("src", "assets/" + notesLeft[randomNum]);
